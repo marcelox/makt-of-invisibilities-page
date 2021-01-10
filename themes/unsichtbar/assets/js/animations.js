735e8e7a47cae9b7bafc798aca9673d12f29eee6
marcelo cordeiro <hi@marcelo.berlin>
2021-01-10 14:02:19 +0100

    Meet WCAG21 2.2.2. success criterion and let user stop animation of movable nav items

--- themes/unsichtbar/assets/js/animations.js
+++ themes/unsichtbar/assets/js/animations.js
@@ -17,3 +17,25 @@
 //
 //     window.requestAnimationFrame(move)
 // }
+
+let show_animation = true;
+const animated_nav_items = document.querySelector('.animated'),
+      stop_animation = document.querySelector('.accessible-nav'),
+      animation_messages = {
+        stop: document.querySelector('.stop-animation'),
+        restart: document.querySelector('.restart-animation'),
+};
+
+const toggle_animation = () => {
+    animated_nav_items.classList.toggle('circles');
+    show_animation = !show_animation;
+    if (show_animation) {
+        animation_messages.restart.classList.remove('visible');
+        animation_messages.stop.classList.add('visible');
+    } else {
+        animation_messages.stop.classList.remove('visible');
+        animation_messages.restart.classList.add('visible');
+    }
+};
+
+stop_animation?.addEventListener('click', toggle_animation);
